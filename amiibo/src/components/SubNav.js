03d5bc38216ccccadaaf5486916a05edb35c2d76
Krunal Patel <Pkrunal616@gmail.com>
2020-06-09 23:46:15 -0400

    adding sub nav

---
+++ amiibo/src/components/SubNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SubNav = (props) => {
+	return (
+		<div>
+			<button>All</button>
+			<button>Mario</button>
+			<button>Pokemon</button>
+			<button>Zelda</button>
+			<button>Animal Crossing</button>
+		</div>
+	);
+};
+
+export default SubNav;
